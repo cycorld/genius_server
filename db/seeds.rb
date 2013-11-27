ff88af02ea577ee5cfdd34382a18217481743af0
likelion-dev <likelion.dev@gmail.com>
2013-11-27 22:53:15 +0900

    user images has been added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "이두희", filename: "ldh") #1
+User.create(name: "홍진호", filename: "hjh") #2
+User.create(name: "이상민", filename: "lsm") #3
+User.create(name: "임윤선", filename: "lys") #4
+User.create(name: "조유영", filename: "jyy") #5
+User.create(name: "노홍철", filename: "nhc") #6
+User.create(name: "은지원", filename: "ejw") #7
+User.create(name: "유정현", filename: "yjh") #8
+User.create(name: "임요환", filename: "lyh") #9
+
