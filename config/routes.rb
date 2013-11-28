587d7e8e33441fa3e3736550a4130044b00f60a9
Charles <charleschoi87@gmail.com>
2013-11-28 17:54:30 +0900

    fixed routes for client

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 GServer::Application.routes.draw do
   root 'web#index'
+	match 'mobile' => "mobile#index", via: [:post]
 	match 'mobile/:id' => "mobile#index", via: [:get, :post]
+	match 'web/push' => "web#push", via: [:post]
   match 'push_message' => "web#push", via: [:get, :post]
   match 'receive_message' => "web#receive_message", via: [:get, :post]
   match 'get_users' => "web#get_users", via: [:get, :post]
