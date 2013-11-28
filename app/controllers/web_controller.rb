0bad68434e12fbcea2d47983ae74db9253737b28
likelion-dev <likelion.dev@gmail.com>
2013-11-28 13:32:33 +0900

    web push

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -1,27 +1,47 @@
 class WebController < ApplicationController
+  before_filter :is_admin, :except => [:login, :login_process]
   def index
-
+    @msgs = Msg.where('send_id = ? OR recv_id = ?', params[:target_user_id], params[:target_user_id]).where(:deleted => false).order("created_at DESC")
   end
 
-  def flush
+  def flush #Ajax call
     send_msgs = Msg.where(:send_id => params[:target_user_id])
     send_msgs << Msg.where(:recv_id => params[:target_user_id])
-
+    send_msgs.each do |x|
+      x.deleted = true
+      x.save
+    end
+    render :json => {"result" => true}
   end
 
   def push #Ajax call
+    msg = Msg.new
+    msg.send_id = 1 #HARD CODED admin ID
+    msg.recv_id = params[:target_user_id]
+    msg.content = params[:content]
+    msg.save
+    #sender #receiver
+    ['1', "#{target_user_id}"].each do |x|
+      Pusher[x].trigger('new_msg', {
+        content: params[:content],
+        send_id: 1
+        recv_id: params[:target_user_id]
+      })
+    end
 
+    render :json => {"result" => true}
   end
 
   def login
-    if !session[:user_id].nil?
-      redirect_to :action => "index"
-    end
   end
 
   def login_process
-    session[:user_id] = params[:user_id]
-    redirect_to :action => "index"
+    if (params[:user_name] == 'admin') and (params[:user_password] == 'tjdbrlfhdhtpdy')
+      session[:admin] = true
+      redirect_to :action => "index"
+    else
+      redirect_to :action => "login"
+    end
   end
 
   def logout
