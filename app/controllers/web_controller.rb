cc3c778d8e840fb0a10723775717aea1d1efa424
hogoo <sang2087@gmail.com>
2013-11-29 22:22:29 +0900

    change images center

--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -25,7 +25,7 @@ class WebController < ApplicationController
     if params[:content][0] == '/'
       puts "hi"
       command = params[:content].split('/')[1]
-      content = %Q[<img src='/images/#{command}.png' style='width:125px;height:125px' />]
+      content = %Q[<center><img src='/images/#{command}.png' style='width:125px;height:125px' /></center>]
       msg.command = true
     end
     msg.content = content
